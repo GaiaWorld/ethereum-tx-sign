a77bbfe2e2ddc4886ff75caecbe8a7a9e7459ce9
jayphbee <jayphbee@163.com>
2019-07-23 13:00:10 +0800

    return txhash and raw tx

--- src/raw_transaction.rs
+++ src/raw_transaction.rs
@@ -25,7 +25,7 @@ pub struct RawTransaction {
 
 impl RawTransaction {
     /// Signs and returns the RLP-encoded transaction
-    pub fn sign(&self, private_key: &H256,chain_id : &u8) -> Vec<u8> {
+    pub fn sign(&self, private_key: &H256,chain_id : &u8) -> (Vec<u8>, Vec<u8>) {
         let hash = self.hash(*chain_id);
         let sig = ecdsa_sign(&hash, &private_key.0, &chain_id);
         let mut tx = RlpStream::new(); 
@@ -35,10 +35,13 @@ impl RawTransaction {
         tx.append(&sig.r); 
         tx.append(&sig.s); 
         tx.complete_unbounded_list();
-        tx.out()
+
+        let serialized = tx.out();
+        // return txhash and serilized tx
+        (keccak256_hash(&serialized), serialized)
     }
 
-    pub fn hash(&self, chain_id: u8) -> Vec<u8> {
+    fn hash(&self, chain_id: u8) -> Vec<u8> {
         let mut hash = RlpStream::new(); 
         hash.begin_unbounded_list();
         self.encode(&mut hash);
